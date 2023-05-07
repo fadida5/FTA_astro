697f2866328ed10158e7215809ff593b5da7dc97
fadida5 <fadidaniv5@gmail.com>
2023-05-08 00:04:27 +0300

    getting started

---
+++ src/components/clock.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+
+export function Clockcomp() {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Current time:</p>
+      <Clock value={value} />
+    </div>
+  );
+}
